6d751533ddb83f08378f00094e99ce3d6397ccff
Federico Iza <fedegiza@gmail.com>
2025-06-19 17:07:08 -0300

    Update figures.

--- src/authors.py
+++ src/authors.py
@@ -3,11 +3,15 @@ import numpy as np
 import pandas as pd
 from typing import List
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 
 from settings import Settings
 
-plt.style.use('seaborn-v0_8-pastel')
+font_path = r"C:\Windows\Fonts\dubai-regular.ttf"
+font_prop = fm.FontProperties(fname=font_path)
 
+DIMGRAY = "#5f5f5f"
+LIGHTGRAY = "#919191"
 QUERY = """
     SELECT
         R.WorkID,
@@ -51,8 +55,28 @@ class AuthorsAnalysis:
                 "PageCount"].sum().reset_index()
         return g.sort_values("PageCount", ascending=False).iloc[:amount]
 
-    def get_highest_rated_authors(self, amount: int = 10) -> None:
-        pass
+    def get_highest_rated_authors(self,
+                                  min_pages_read: int = 1000) -> pd.DataFrame:
+        df_finished = self.df[self.df["Status"] == "FINISHED"]
+
+        def compute_stats(x):
+            weights = x["PageCount"]
+            scores = x["Score"]
+            weighted_mean = np.average(scores, weights=weights)
+            variance = np.average((scores - weighted_mean) ** 2,
+                                  weights=weights)
+            weighted_std = np.sqrt(variance)
+            return pd.Series({
+                "TotalPages": weights.sum(),
+                "WeightedScore": weighted_mean,
+                "WeightedStd": weighted_std
+            })
+
+        g = df_finished.groupby("Author").apply(compute_stats).reset_index()
+        g = g[g["TotalPages"] >= min_pages_read]
+        g = g.sort_values("WeightedScore", ascending=False)
+        g.min_pages_read = min_pages_read
+        return g
 
     def get_author_read(self) -> None:
         pass
@@ -61,66 +85,96 @@ class AuthorsAnalysis:
 def plot_most_read_authors(df: pd.DataFrame) -> None:
     df = df.sort_values("PageCount", ascending=True).reset_index(drop=True)
 
-    minor_ticks = []
-    for base in range(1000, 25000, 5000):
-        for offset in range(0, 5000, 1000):
-            if offset == 4000:
-                continue
-            minor_ticks.append(base + offset)
-
-    fig = plt.figure(figsize=(2.5, 3.5))
+    fig = plt.figure(figsize=(1.8, 1.8))
     gs = fig.add_gridspec(ncols=1, nrows=1, hspace=0, wspace=0)
-    ax = gs.subplots(sharex=True, sharey=True)
+    ax = fig.add_subplot(gs[0, 0])
 
     ax.set_ylim(-0.5, len(df) - 0.5)
+    ax.set_yticks([i for i in range(len(df))])
+    ax.set_yticklabels(df["Author"])
+
     ax.set_xlim(0, 25000)
     ax.set_xticks([0, 5000, 10000, 15000, 20000, 25000])
-    ax.set_xticks(minor_ticks, minor=True)
-    ax.set_xticklabels([0, "5k", "10k", "15k", "20k", "25k"])
+    ax.set_xticklabels(["0", "5k", "10k", "15k", "20k", "25k"])
 
     ax.xaxis.tick_top()
     ax.xaxis.set_label_position('top')
-    ax.tick_params(axis="x", which="major", length=2.5, width=0.7, labelsize=4)
-    ax.tick_params(axis="x", which="minor", length=2.5, width=0.2, labelsize=5)
-    ax.tick_params(axis="y", which="both", labelsize=4.5, length=0)
-    for tick in ax.xaxis.get_minorticklines():
-        tick.set_alpha(0.3)
-    for spine in ["right", "left", "top", "bottom"]:
+    ax.tick_params(axis="x", which="both", length=0, width=0.4,
+                   labelsize=3.5, color=LIGHTGRAY, labelcolor=LIGHTGRAY)
+    ax.tick_params(axis="y", which="both", labelsize=3.5, length=0,
+                   labelcolor=LIGHTGRAY)
+    for spine in ["right", "left", "bottom"]:
         ax.spines[spine].set_visible(False)
+    ax.spines["top"].set_color(LIGHTGRAY)
+    ax.spines["top"].set_linewidth(0.4)
 
-    ax.barh(df["Author"], df["PageCount"], zorder=10, height=0.7)
+    ax.scatter(df["PageCount"], [i for i in range(len(df))], zorder=10,
+               color=DIMGRAY, s=5, facecolor="none", linewidths=0.5)
 
-    # Add author names and page count values as labels
     for i in range(len(df["Author"])):
         pages_read = df["PageCount"].iloc[i]
+        ax.text(pages_read, i - 0.5, pages_read, zorder=11, size=2,
+                va="center", ha="center", color=DIMGRAY)
 
-        ax.text(pages_read + 200, i, pages_read, zorder=11, size=4,
-                va="center", ha="left")
-
-        x_pos = 1000
-        while x_pos <= df["PageCount"].max():
-            if pages_read >= x_pos:
-                ax.plot([x_pos] * 2, [i - 0.7 / 2, i + 0.7 / 2],
-                        color="white", lw=0.2, zorder=12, alpha=0.5)
-            x_pos += 1000
-
-        x_pos = 5000
-        while x_pos <= df["PageCount"].max():
-            if pages_read >= x_pos:
-                ax.plot([x_pos] * 2, [i - 0.7 / 2, i + 0.7 / 2],
-                        color="white", lw=0.7, zorder=12)
-            x_pos += 5000
-
-    plt.text(0.3, 0.5, "Most Read Authors", fontsize=10, va="top",
-             transform=ax.transAxes)
     plt.text(
-        0.3, 0.44, va="top", fontsize=5.5, transform=ax.transAxes,
-        s="Visualizing the most read authors\nby the amount of pages read.",
+        -0.4, 1.15, va="bottom", ha="left", fontsize=5.5,
+        transform=ax.transAxes, color=LIGHTGRAY,
+        s=f"Top {len(df)} Most Read Authors by Page Count",
     )
 
-    fig.savefig("images/most_read_authors.pdf", dpi=300, bbox_inches="tight")
+    fig.savefig("images/most_read_authors.png", dpi=500, bbox_inches="tight")
+
+
+def plot_highest_rated_authors(df: pd.DataFrame) -> None:
+    df_filtered = df.sort_values(
+        "WeightedScore", ascending=True).reset_index(
+            drop=True)
+
+    fig = plt.figure(figsize=(1.8, 1.8))
+    gs = fig.add_gridspec(ncols=1, nrows=1, hspace=0, wspace=0)
+    ax = fig.add_subplot(gs[0, 0])
+
+    ax.set_ylim(-0.5, len(df_filtered) - 0.5)
+    ax.set_yticks([i for i in range(len(df_filtered))])
+    ax.set_yticklabels(df_filtered["Author"])
+
+    ax.set_xlim(0, 5)
+    ax.set_xticks([0, 1, 2, 3, 4, 5])
+
+    ax.xaxis.tick_top()
+    ax.xaxis.set_label_position('top')
+    ax.tick_params(axis="x", which="both", length=0, width=0.4,
+                   labelsize=3.5, color=LIGHTGRAY, labelcolor=LIGHTGRAY)
+    ax.tick_params(axis="y", which="both", labelsize=3.5, length=0,
+                   labelcolor=LIGHTGRAY)
+    for spine in ["right", "left", "bottom"]:
+        ax.spines[spine].set_visible(False)
+    ax.spines["top"].set_color(LIGHTGRAY)
+    ax.spines["top"].set_linewidth(0.4)
+
+    ax.scatter(df_filtered["WeightedScore"],
+               [i for i in range(len(df_filtered))],
+               zorder=10, color=DIMGRAY, s=5, facecolor="none",
+               linewidths=0.5)
+
+    for i in range(len(df_filtered["Author"])):
+        weighted_score = df_filtered["WeightedScore"].iloc[i]
+        ax.text(weighted_score, i - 0.5,
+                str(round(weighted_score, 2)).ljust(4, "0"),
+                zorder=11, size=2, va="center", ha="center", color=DIMGRAY)
+
+    plt.text(
+        -0.4, 1.15, va="bottom", ha="left", fontsize=5.5,
+        transform=ax.transAxes, color=LIGHTGRAY,
+        s=f"Scores of Authors with {df.min_pages_read}+ Pages Read",
+    )
+
+    fig.savefig("images/highest_rated_authors.png",
+                dpi=500, bbox_inches="tight")
 
 
 if __name__ == "__main__":
     a = AuthorsAnalysis(Settings.DATABASE_PATH)
-    plot_most_read_authors(df=a.get_most_read_authors(25))
+    plot_most_read_authors(df=a.get_most_read_authors(15))
+    plot_highest_rated_authors(
+        df=a.get_highest_rated_authors(min_pages_read=1500))
