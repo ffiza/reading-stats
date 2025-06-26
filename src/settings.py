7f1a3481f17b0a33034255e60d6a24d5694135f7
Federico Iza <fedegiza@gmail.com>
2025-06-26 19:21:23 -0300

    Add dictionary for plural work types.

--- src/settings.py
+++ src/settings.py
@@ -1,2 +1,13 @@
 class Settings:
     DATABASE_PATH = "data/raw/books.db"
+
+    PLURAL_WORK_NAME: dict = {
+        "Novel": "Novels",
+        "Short Story": "Short Stories",
+        "Novella": "Novellas",
+        "Novelette": "Novelettes",
+        "Non-Fiction": "Non-Fiction Books",
+        "Graphic Novel": "Graphic Novels",
+        "Anthology": "Anthologies",
+        "Poetry": "Poems",
+    }
