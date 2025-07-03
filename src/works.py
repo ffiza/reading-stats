3c40919441810e5661ab659cbb76c037e7fc546a
Federico Iza <fedegiza@gmail.com>
2025-07-03 20:46:29 -0300

    Update figure.

--- src/works.py
+++ src/works.py
@@ -47,58 +47,58 @@ class WorksAnalysis:
         return list(self.df[self.df["Name"] == title]["Author"].unique())
 
     def plot_highest_rated_works(self, work_type: str) -> None:
-        N_WORKS: int = 10
-        TEXT_OFFSET: float = -0.03
+        N_WORKS: int = 20
+        SEPARATION: float = 0.5
         df = self.get_unique_works()
         df = df[df["Type"] == work_type]
         df = df.reset_index()
 
-        fig = plt.figure(figsize=(1.8, 1.8))
+        fig = plt.figure(figsize=(1.8, 2.5))
         gs = fig.add_gridspec(ncols=1, nrows=1, hspace=0, wspace=0)
         ax = fig.add_subplot(gs[0, 0])
 
-        ax.set_ylim(-0.5, N_WORKS - 0.5)
+        ax.set_ylim(-0.6, 29.1)
         ax.set_yticks([])
         ax.set_xlim(-0.25, 5)
         ax.set_xticks([])
+        plt.gca().invert_yaxis()
 
         for spine in ["right", "left", "bottom", "top"]:
             ax.spines[spine].set_visible(False)
 
         for i in range(N_WORKS):
-            j = N_WORKS - i
-            ax.scatter(0, i, s=20, color=Colors.DARKGRAY, zorder=20)
-            ax.text(x=0, y=i + TEXT_OFFSET, s=str(j), ha="center", va="center",
-                    color=Colors.WHITE, size=3, weight=600, zorder=21,
-                    fontname=Settings.FONTNAME,)
-            name = df.loc[j]['Name'].replace(' ', '\\ ').replace(
+            ax.annotate(
+                str(i + 1), xy=(0.02, i + SEPARATION * i),
+                ha="center", va="center", color=Colors.DARKGRAY,
+                size=4, weight=800, zorder=21, fontname=Settings.FONTNAME)
+            name = df.loc[i]['Name'].replace(' ', '\\ ').replace(
                 "-", r"\text{-}")
-            year = df.loc[j]['PublishedOn']
-            ax.text(x=0.4, y=i + TEXT_OFFSET, s=f"$\\bf{{{name}}}$ ({year})",
-                    ha="left", va="center", color=Colors.DARKGRAY, size=4,
+            year = df.loc[i]['PublishedOn']
+            ax.text(x=0.2, y=i + SEPARATION * i,
+                    s=f"$\\bf{{{name}}}$ ({year})",
+                    ha="left", va="bottom", color=Colors.DARKGRAY, size=3.5,
                     fontname=Settings.FONTNAME,)
-            authors = self.get_work_authors_list(str(df.loc[j]['Name']))
+            authors = self.get_work_authors_list(str(df.loc[i]['Name']))
             authors_text = " & ".join(authors)
-            ax.text(x=0.4, y=i - 0.4, s=authors_text, style='italic',
-                    ha="left", va="center", color=Colors.DARKGRAY, size=3.5,
-                    fontname=Settings.FONTNAME,)
+            ax.text(x=0.2, y=i + SEPARATION * i, s=authors_text,
+                    style='italic', fontname=Settings.FONTNAME,
+                    ha="left", va="top", color=Colors.DARKGRAY, size=3)
 
         plt.text(
-            0, 1.02, va="bottom", ha="left", fontsize=Settings.TITLE_FONTSIZE,
+            0.02, 1.06, va="bottom", ha="left", fontsize=6,
             transform=ax.transAxes, color=Colors.DARKGRAY,
-            s=f"Top {N_WORKS} Highest Rated "
-              f"{Settings.PLURAL_WORK_NAME[work_type]}",
-            fontname=Settings.FONTNAME,
+            s=f"Top 20 Highest Rated {Settings.PLURAL_WORK_NAME[work_type]}",
+            fontname=Settings.FONTNAME, weight=800,
         )
 
+        plt.subplots_adjust(left=0, right=1, bottom=0)
         fig.savefig(
             "images/highest_rated_"
             f"{Settings.PLURAL_WORK_NAME[work_type].lower().replace(' ', '_')}"
-            ".png",
-            dpi=500, bbox_inches="tight")
+            ".png", dpi=500)
 
 
 if __name__ == "__main__":
     a = WorksAnalysis(Settings.DATABASE_PATH)
     a.plot_highest_rated_works(work_type="Novel")
-    a.plot_highest_rated_works(work_type="Short Story")
+    # a.plot_highest_rated_works(work_type="Short Story")
