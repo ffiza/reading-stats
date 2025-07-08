5c5507e336defdaac702fcbdc54afee753774d92
Federico Iza <fedegiza@gmail.com>
2025-07-08 08:27:42 -0300

    Add methodo to transform from ISO3 to ISO2 country codes.

--- src/utils.py
+++ src/utils.py
@@ -26,3 +26,24 @@ def flag_from_iso3(iso3: str) -> str:
         return ''.join(chr(ord(char) + 127397) for char in iso2)
     except Exception as e:
         return f"Error: {e}"
+
+
+def iso3_to_iso2(iso3_code: str) -> str:
+    """
+    Convert an ISO 3166-1 alpha-3 country code to its corresponding
+    alpha-2 code.
+
+    Parameters
+    ----------
+    iso3_code : str
+        The 3-letter ISO 3166-1 alpha-3 country code (e.g., 'ARG', 'USA').
+
+    Returns
+    -------
+    str
+        The 2-letter ISO 3166-1 alpha-2 country code.
+    """
+    country = pycountry.countries.get(alpha_3=iso3_code.upper())
+    if country is None:
+        raise ValueError("Country not found.")
+    return country.alpha_2
