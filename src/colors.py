b655c0268ed1208f111e223d90b0d10a00e9db9a
Federico Iza <fedegiza@gmail.com>
2026-02-28 17:37:44 -0300

    Add new color palette.

--- src/colors.py
+++ src/colors.py
@@ -3,3 +3,10 @@ class Colors:
     DARKGRAY = "#1f2328"
     MEDIUMGRAY = "#646464"
     LIGHTGRAY = "#f0f0f0"
+
+    PURPLE = "#b577b0"
+    GREEN = "#339d98"
+    BLUE = "#7088b0"
+    ORANGE = "#ea8b7b"
+    RED = "#a05961"
+    CYAN = "#60bbc8"
