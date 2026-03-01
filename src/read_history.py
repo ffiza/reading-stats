f3f692af1252b31f0d3f2eb38d6ec207b8c165a9
Federico Iza <fedegiza@gmail.com>
2026-02-28 23:20:48 -0300

    Move query to its own class.

--- src/read_history.py
+++ src/read_history.py
@@ -2,40 +2,12 @@ import sqlite3
 import pandas as pd
 
 from settings import Settings
-
-
-QUERY = """
-    SELECT
-        A.Name                AS AuthorName,
-        A.AuthorID            AS AuthorID,
-        W.Name                AS WorkName,
-        W.WorkType            AS WorkType,
-        W.Series              AS Series,
-        W.NumberInSeries      AS NumberInSeries,
-        W.PublishedOn         AS PublishedOn,
-        W.Genre               AS Genre,
-        W.PageCount           AS PageCount,
-        R.Score               AS ReadScore,
-        W.GoodreadsScore      AS GoodreadsScore,
-        W.WorkID              AS WorkID,
-        R.StartDate           AS StartDate,
-        R.FinishDate          AS FinishDate,
-        R.Status              AS ReadStatus
-    FROM READS R
-    JOIN WORKS W
-        ON R.WorkID = W.WorkID
-    JOIN AUTHOR_WORK AW
-        ON W.WorkID = AW.WorkID
-    JOIN AUTHORS A
-        ON AW.AuthorID = A.AuthorID
-    WHERE R.Status NOT IN ('NEXT')
-    ORDER BY A.Name, W.Name, R.StartDate;
-    """
+from queries import Queries
 
 
 def export_read_history(database_path: str) -> None:
     conn = sqlite3.connect(database_path)
-    df = pd.read_sql(QUERY, conn)
+    df = pd.read_sql(Queries.READ_HISTORY, conn)
     conn.close()
     df.to_csv("data/processed/read_history.csv", index=False)
 
