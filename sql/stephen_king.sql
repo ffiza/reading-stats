5454a5758fa88e45ebbb01317beb4c167811a97c
Federico Iza <fedegiza@gmail.com>
2026-03-01 12:23:16 -0300

    Add script to export Stephen King bibliography to `README.md`.

---
+++ sql/stephen_king.sql
@@ -0,0 +1,25 @@
+SELECT
+    A.Name                AS AuthorName,
+    A.AuthorID            AS AuthorID,
+    W.Name                AS WorkName,
+    W.WorkType            AS WorkType,
+    W.Series              AS Series,
+    W.NumberInSeries      AS NumberInSeries,
+    W.PublishedOn         AS PublishedOn,
+    W.Genre               AS Genre,
+    W.PageCount           AS PageCount,
+    R.Score               AS ReadScore,
+    W.GoodreadsScore      AS GoodreadsScore,
+    W.WorkID              AS WorkID,
+    R.StartDate           AS StartDate,
+    R.FinishDate          AS FinishDate,
+    R.Status              AS ReadStatus
+FROM WORKS W
+LEFT JOIN READS R
+    ON R.WorkID = W.WorkID
+JOIN AUTHOR_WORK AW
+    ON W.WorkID = AW.WorkID
+JOIN AUTHORS A
+    ON AW.AuthorID = A.AuthorID
+WHERE A.Name = "Stephen King"
+ORDER BY A.Name, W.Name, R.StartDate;
